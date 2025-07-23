27821f1e1f1761fb4e8fd4f6d32f3a7eea019005
Diogo <diogocrmaia03@gmail.com>
2025-07-23 20:04:57 -0300

    atualização

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: true, // ou '0.0.0.0' para expor em todos os endereços
+    port: process.env.PORT || 3000, // Usa a porta do Render ou 3000 localmente
+    strictPort: true // Fecha se a porta estiver em uso
+  },
+  preview: {
+    port: process.env.PORT || 3000 // Configuração para o preview (build)
+  }
 });
